da4694d01436a2739d2c1ea6d3ebf323ab070584
samrhatSunny <81028930+samrhatSunny@users.noreply.github.com>
2025-05-08 16:24:26 +0530

    Add files via upload

---
+++ NumFact.py
@@ -0,0 +1,6 @@
+
+def fact(num):
+    sum = 1
+    for i in range(1,num+1):
+        sum = sum * i
+    return sum
---
+++ main.py
@@ -0,0 +1,17 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import NumFact, NumCalculation
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    num = int(input("Enter the number"))
+    no = NumFact.fact(num)
+    print("The Factioral of",num,"is",no)
+    num1 = int(input("Enter the number"))
+    NumCalculation.numMath(num1)
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
